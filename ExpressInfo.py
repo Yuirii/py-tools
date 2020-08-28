68502365f654b7ff58d738e9d11a2cf80292969b
yuirii <hyl27481847@vip.qq.com>
2020-08-28 15:11:23 +0800

    Success to fulfill basic function.

--- ExpressInfo.py
+++ ExpressInfo.py
@@ -9,6 +9,18 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QPushButton
+import jieba.posseg
+import jieba.analyse
+from openpyxl import Workbook
+
+
+# #test mode
+class Person:
+    type = []
+    name = []
+    phone = []
+    context = []
+    pass
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -35,6 +47,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.gridLayout.addWidget(self.read_label, 2, 1, 1, 1)
         self.Startbtn = QtWidgets.QPushButton(self.centralwidget)
         self.Startbtn.setObjectName("Startbtn")
+        self.Startbtn.setFixedHeight(50)
         self.gridLayout.addWidget(self.Startbtn, 4, 1, 1, 1)
         self.file_ads_edit = QtWidgets.QLineEdit(self.centralwidget)
         self.file_ads_edit.setObjectName("file_ads_edit")
@@ -54,13 +67,73 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.btn_clicked()
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.file_ads_label.setText(_translate("MainWindow", "文件地址："))
-        self.read_label.setText(_translate("MainWindow", "识别信息："))
+        MainWindow.setWindowTitle(_translate("MainWindow", "快递信息制表V1.0"))
+        self.file_ads_label.setText(_translate("MainWindow", "文件输出地址："))
+        self.read_label.setText(_translate("MainWindow", "待识别信息："))
         self.Startbtn.setText(_translate("MainWindow", "开始识别并写入"))
-        self.write_label.setText(_translate("MainWindow", "写入信息："))
+        self.write_label.setText(_translate("MainWindow", "已写入信息："))
+
+    def btn_clicked(self):
+
+
+        def cao():
+
+            fileads_str = self.file_ads_edit.text()
+            express_str =self.read_edit.toPlainText().replace('\r', '').replace('\n', '').replace('\t', '').replace(' ','').replace(',','').replace('，','')
+            print(fileads_str,express_str)
+            # process data
+            person_per = Person()
+
+            # handle message
+            words = jieba.posseg.cut(express_str)
+            index = 0
+            for word, flag in words:
+                # print('%s %s' % (word, flag))
+                if flag == "eng":
+                    person_per.type = word
+                if flag == "nr" and word != "阿玛尼":
+                    person_per.name = word
+                if flag == "m" and len(word) == 11:
+                    person_per.phone = word
+                if flag != "nr" and len(word) != 11:
+                    person_per.context += word
+                    print('%d %s' % (index, person_per.context))
+            #
+            print("type", person_per.type)
+            print("name", person_per.name)
+            print("phone", person_per.phone)
+            context1 = ''.join([str(i) for i in person_per.context])
+            print("context", context1)
+
+            # echo message
+            self.write_display.setText('型号：'+str(person_per.type)+'\n' \
+                                       +'姓名：'+str(person_per.name)+'\n'    \
+                                       +'电话：'+str(person_per.phone)+'\n'    \
+                                       +'地址及备注：'+context1)
+            # echo to excel
+            wb = Workbook()
+            ws = wb.active
+            ws.append(['型号', '姓名', '手机', '地址及备注'])
+            ws.append([str(person_per.type), str(person_per.name), str(person_per.phone), context1])
+            wb.save(fileads_str + ".xlsx")
+
+             # clear message
+            person_per.type = 0
+            person_per.name = 0
+            person_per.phone = 0
+            person_per.context[:] = []
+
+
+
+            # ???
+            print('cao end.')
+
+        self.Startbtn.clicked.connect(cao)
+        print('dataprocess_btn end.')
 
 if __name__ == '__main__':
     import sys
